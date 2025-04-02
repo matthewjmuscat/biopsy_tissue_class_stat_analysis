157f648e5cbdd3c45e0993f8ad89d93853c84be6
Matthew Muscat <matthew.muscat@mail.mcgill.ca>
2025-04-02 01:58:03 -0700

    all radiomic features and biopsy features done for tissue class paper

--- main_pipe.py
+++ main_pipe.py
@@ -3,11 +3,14 @@ import load_files
 from pathlib import Path
 import os 
 import statistical_tests_1_quick_and_dirty
+import shape_and_radiomic_features
+import misc_funcs
+import biopsy_information
 
 def main():
     
     # Main output (files, input) directory
-    main_output_path = Path("/home/mjm/Documents/UBC/Research/biopsylocalization-new/Data/Output data/MC_sim_out- Date-Jan-14-2025 Time-14,25,24")  # Ensure the directory is a Path object
+    main_output_path = Path("/home/matthew-muscat/Documents/UBC/Research/Data/Output data/MC_sim_out- Date-Apr-01-2025 Time-15,04,17")  # Ensure the directory is a Path object
     
 
 
@@ -17,17 +20,206 @@ def main():
     csv_directory = main_output_path.joinpath("Output CSVs")
     cohort_csvs_directory = csv_directory.joinpath("Cohort")
 
-    # Cohort: Global dosimetry dataframe
-    cohort_global_sum_to_one_tissue_path = cohort_csvs_directory.joinpath("Cohort: global sum-to-one mc results_custom.csv")  # Ensure the directory is a Path object
+    # Cohort 3d radiomic features all oar and dil structures
+    cohort_3d_radiomic_features_all_oar_dil_path = cohort_csvs_directory.joinpath("Cohort: 3D radiomic features all OAR and DIL structures.csv")  # Ensure the directory is a Path object
+    cohort_3d_radiomic_features_all_oar_dil_df = load_files.load_csv_as_dataframe(cohort_3d_radiomic_features_all_oar_dil_path)  # Load the CSV file into a DataFrame
+    """ NOTE: The columns of the dataframe are:
+    cohort_3d_radiomic_features_all_oar_dil_df.columns =
+    Index(['Patient ID', 'Structure ID', 'Structure type', 'Structure refnum',
+       'Volume', 'Surface area', 'Surface area to volume ratio', 'Sphericity',
+       'Compactness 1', 'Compactness 2', 'Spherical disproportion',
+       'Maximum 3D diameter', 'PCA major', 'PCA minor', 'PCA least',
+       'PCA eigenvector major', 'PCA eigenvector minor',
+       'PCA eigenvector least', 'Major axis (equivalent ellipse)',
+       'Minor axis (equivalent ellipse)', 'Least axis (equivalent ellipse)',
+       'Elongation', 'Flatness', 'L/R dimension at centroid',
+       'A/P dimension at centroid', 'S/I dimension at centroid',
+       'S/I arclength', 'DIL centroid (X, prostate frame)',
+       'DIL centroid (Y, prostate frame)', 'DIL centroid (Z, prostate frame)',
+       'DIL centroid distance (prostate frame)', 'DIL prostate sextant (LR)',
+       'DIL prostate sextant (AP)', 'DIL prostate sextant (SI)'],
+      dtype='object')
+    """
+    
+    
+    
+    # biopsy basic spatial features
+    cohort_biopsy_basic_spatial_features_path = cohort_csvs_directory.joinpath("Cohort: Biopsy basic spatial features dataframe.csv")  # Ensure the directory is a Path object
+    cohort_biopsy_basic_spatial_features_df = load_files.load_csv_as_dataframe(cohort_biopsy_basic_spatial_features_path)  # Load the CSV file into a DataFrame
+    """ NOTE: The columns of the dataframe are:
+    cohort_biopsy_basic_spatial_features_df.columns =
+    Index(['Patient ID', 'Bx ID', 'Simulated bool', 'Simulated type',
+       'Struct type', 'Bx refnum', 'Bx index', 'Length (mm)', 'Volume (mm3)',
+       'Voxel side length (mm)', 'Relative DIL ID', 'Relative DIL index',
+       'BX to DIL centroid (X)', 'BX to DIL centroid (Y)',
+       'BX to DIL centroid (Z)', 'BX to DIL centroid distance',
+       'NN surface-surface distance', 'Relative prostate ID',
+       'Relative prostate index', 'Bx position in prostate LR',
+       'Bx position in prostate AP', 'Bx position in prostate SI'],
+      dtype='object')
+      """
+
+
+    # Cohort: Global sum-to-one mc results
+    cohort_global_sum_to_one_tissue_path = cohort_csvs_directory.joinpath("Cohort: global sum-to-one mc results.csv")  # Ensure the directory is a Path object
     cohort_global_sum_to_one_tissue_df = load_files.load_csv_as_dataframe(cohort_global_sum_to_one_tissue_path)
 
+    # Cohort sum-to-one mc results
+    cohort_sum_to_one_mc_results_path = cohort_csvs_directory.joinpath("Cohort: sum-to-one mc results.csv")  # Ensure the directory is a Path object
+    cohort_sum_to_one_mc_results_df = load_files.load_csv_as_dataframe(cohort_sum_to_one_mc_results_path)  # Load the CSV file into a DataFrame
 
-
+    # Cohort tissue class - distances global
+    cohort_tissue_class_distances_global_path = cohort_csvs_directory.joinpath("Cohort: Tissue class - distances global results.csv")  # Ensure the directory is a Path object
+    # this is a multiindex dataframe
+    cohort_tissue_class_distances_global_df = load_files.load_multiindex_csv(cohort_tissue_class_distances_global_path, header_rows=[0, 1])  # Load the CSV file into a DataFrame
 
     ## Create output directory
     # Output directory 
     output_dir = Path(__file__).parents[0].joinpath("output_data")
     os.makedirs(output_dir, exist_ok=True)
+
+
+    ### Get unqiue patient IDs
+    # Get ALL unique patient IDs from the cohort_3d_radiomic_features_all_oar_dil_df DataFrame
+    unique_patient_ids_all = cohort_biopsy_basic_spatial_features_df['Patient ID'].unique().tolist()
+    # Print the unique patient IDs
+    print("Unique Patient IDs (ALL):")
+    print(unique_patient_ids_all)
+    # Print the number of unique patient IDs
+    print(f"Number of unique patient IDs ALL: {len(unique_patient_ids_all)}")
+
+
+    # Get unique patient IDs, however the patient IDs actually include the patient ID (F#) where F# indicates the fraction but its actually the same patient, so I want to take only F1, if F1 isnt present for a particular ID then I want to take F2
+    # Get the unique patient IDs from the cohort_3d_radiomic_features_all_oar_dil_df DataFrame
+    unique_patient_ids_f1_prioritized = misc_funcs.get_unique_patient_ids_fraction_prioritize(cohort_biopsy_basic_spatial_features_df,patient_id_col='Patient ID', priority_fraction='F1')
+    # Print the unique patient IDs
+    print("Unique Patient IDs (F1) prioritized:")
+    print(unique_patient_ids_f1_prioritized)
+    # Print the number of unique patient IDs
+    print(f"Number of unique patient IDs (F1) prioritized: {len(unique_patient_ids_f1_prioritized)}")
+
+    ### Get unique patient IDs for F1
+    # Get the unique patient IDs from the cohort_3d_radiomic_features_all_oar_dil_df DataFrame
+    unique_patient_ids_f1 = misc_funcs.get_unique_patient_ids_fraction_specific(cohort_biopsy_basic_spatial_features_df, patient_id_col='Patient ID',fraction='F1')
+    # Print the unique patient IDs
+    print("Unique Patient IDs (F1) ONLY:")
+    print(unique_patient_ids_f1)
+    # Print the number of unique patient IDs
+    print(f"Number of unique patient IDs F1 ONLY: {len(unique_patient_ids_f1)}")
+
+    ### Get unique patient IDs for F2
+    # Get the unique patient IDs from the cohort_3d_radiomic_features_all_oar_dil_df DataFrame
+    unique_patient_ids_f2 = misc_funcs.get_unique_patient_ids_fraction_specific(cohort_biopsy_basic_spatial_features_df, patient_id_col='Patient ID',fraction='F2')
+    # Print the unique patient IDs
+    print("Unique Patient IDs (F2) only:")
+    print(unique_patient_ids_f2)
+    # Print the number of unique patient IDs
+    print(f"Number of unique patient IDs F2 ONLY: {len(unique_patient_ids_f2)}")
+
+
+
+    ### Radiomic features analysis (START)
+    # Create output directory for radiomic features
+    radiomic_features_dir = output_dir.joinpath("radiomic_features")
+    os.makedirs(radiomic_features_dir, exist_ok=True)
+    # Output filename
+    output_filename = 'radiomic_features_statistics_all_patients.csv'
+    # Get radiomic statistics
+    radiomic_statistics_df = shape_and_radiomic_features.get_radiomic_statistics(cohort_3d_radiomic_features_all_oar_dil_df, 
+                                                                                 patient_id= unique_patient_ids_f1_prioritized, 
+                                                                                 exclude_columns=['Patient ID', 'Structure ID', 'Structure type', 'Structure refnum','PCA eigenvector major', 'PCA eigenvector minor',	'PCA eigenvector least', 'DIL centroid (X, prostate frame)', 'DIL centroid (Y, prostate frame)', 'DIL centroid (Z, prostate frame)', 'DIL centroid distance (prostate frame)', 'DIL prostate sextant (LR)', 'DIL prostate sextant (AP)', 'DIL prostate sextant (SI)'])
+
+    # Save the statistics to a CSV file
+    radiomic_statistics_df.to_csv(radiomic_features_dir.joinpath(output_filename), index=True)
+    # Print the statistics
+    print(radiomic_statistics_df)
+    ### Radiomic features analysis (END)
+
+
+    ### Find DIL double sextant percentages (START)
+    # Create output directory for DIL information
+    dil_information_dir = output_dir.joinpath("dil_information")
+    os.makedirs(dil_information_dir, exist_ok=True)
+    # Output filename
+    output_filename = 'dil_double_sextant_percentages_all_patients.csv'
+    # Get DIL double sextant percentages
+    dil_double_sextant_percentages_df = shape_and_radiomic_features.find_dil_double_sextant_percentages(cohort_3d_radiomic_features_all_oar_dil_df, patient_id=unique_patient_ids_f1_prioritized)
+    # Save the statistics to a CSV file
+    dil_double_sextant_percentages_df.to_csv(dil_information_dir.joinpath(output_filename), index=True)
+    # Print the statistics
+    print(dil_double_sextant_percentages_df)
+    ### Find DIL double sextant percentages (END)
+
+
+    ### Find structure counts (START)
+    # Create output directory for DIL information
+    radiomic_features_dir = output_dir.joinpath("radiomic_features")
+    os.makedirs(radiomic_features_dir, exist_ok=True)
+    # Output filename
+    output_filename = 'structure_counts_all_patients.csv'
+    # Get structure counts
+    structure_counts_df, structure_counts_statistics_df = shape_and_radiomic_features.calculate_structure_counts_and_stats(cohort_3d_radiomic_features_all_oar_dil_df, patient_id=unique_patient_ids_f1_prioritized, structure_types=None)
+    # Save the statistics to a CSV file
+    structure_counts_df.to_csv(radiomic_features_dir.joinpath(output_filename), index=True)
+    # Print the statistics
+    print(structure_counts_df)
+    # Save the statistics to a CSV file
+    output_filename = 'structure_counts_statistics_all_patients.csv'
+    # Save the statistics to a CSV file
+    structure_counts_statistics_df.to_csv(radiomic_features_dir.joinpath(output_filename), index=True)
+    # Print the statistics
+    print(structure_counts_statistics_df)
+    ### Find structure counts (END)
+
+
+    ### Biopsy information analysis (START)
+    # Create output directory for biopsy information
+    biopsy_information_dir = output_dir.joinpath("biopsy_information")
+    os.makedirs(biopsy_information_dir, exist_ok=True)
+    # Output filename
+    output_filename = 'biopsy_information_statistics_all_patients.csv'
+    # Get biopsy information statistics
+    biopsy_information_statistics_df = biopsy_information.get_filtered_statistics(cohort_biopsy_basic_spatial_features_df, 
+                                                                                 columns=['Length (mm)', 
+                                                                                          'Volume (mm3)', 
+                                                                                          'Voxel side length (mm)',  
+                                                                                          'BX to DIL centroid (X)', 
+                                                                                          'BX to DIL centroid (Y)',
+                                                                                          'BX to DIL centroid (Z)', 
+                                                                                          'BX to DIL centroid distance', 
+                                                                                          'NN surface-surface distance'], 
+                                                                                 patient_id=unique_patient_ids_all,
+                                                                                 simulated_type='Real')
+
+    # Save the statistics to a CSV file
+    biopsy_information_statistics_df.to_csv(biopsy_information_dir.joinpath(output_filename), index=True)
+    # Print the statistics
+    print(biopsy_information_statistics_df)
+    ### Biopsy information analysis (END)
+
+
+
+
+    ### Find biopsy double sextant percentages (START)
+    # Create output directory for biopsy information
+    biopsy_information_dir = output_dir.joinpath("biopsy_information")
+    os.makedirs(biopsy_information_dir, exist_ok=True)
+    # Output filename
+    output_filename = 'biopsy_double_sextant_percentages_all_patients.csv'
+    # Get biopsy double sextant percentages
+    biopsy_double_sextant_percentages_df = biopsy_information.find_biopsy_double_sextant_percentages(cohort_biopsy_basic_spatial_features_df, 
+                                                                                                     patient_id=unique_patient_ids_all,
+                                                                                                     simulated_type='Real')
+    # Save the statistics to a CSV file
+    biopsy_double_sextant_percentages_df.to_csv(biopsy_information_dir.joinpath(output_filename), index=True)
+    # Print the statistics
+    print(biopsy_double_sextant_percentages_df)
+    ### Find biopsy double sextant percentages (END)
+
+
+
+
+
 
     statistical_tests_1_dir = output_dir.joinpath("statistical_tests_0")
     os.makedirs(statistical_tests_1_dir, exist_ok=True)
